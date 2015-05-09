00a9e15a73120dc5f357948f4e938c454d11cc6f
francopiccolo <francopiccolo@gmail.com>
2015-05-09 11:25:48 -0300

    enunciados

--- AID/Franco/tp2franco.R
+++ AID/Franco/tp2franco.R
@@ -18,11 +18,28 @@ colMeans(gorriones[,2:ncol(gorriones)])
 cov(gorriones[,2:ncol(gorriones)])
 cor(gorriones[,2:ncol(gorriones)])
 
-#c - La varianza del largo total
+#c - La varianza del largo total/La covarianza del largo total y el largo del pico y cabeza.
 
-#d - La covarianza del largo total y el largo del pico y cabeza.
+#d - La correlación consigo mismo de la extensión alar y la covarianza del largo total y el largo del pico y cabeza. 
 
-#e - La correlación consigo mismo de la extensión alar y 
+#e - m12=m21/sqr(m11)*sqr(m22)
+
+#f
+gorriones2<-gorriones
+gorriones2$diflargos<-gorriones[[2]]-gorriones[[5]]
+
+#g - La media de la nueva variable dif largos es la diferencia entre las medias del largo total y el largo del humero.
+# En el vector de medias vemos la escala de las distintas variables, quiza tendriamos que normalizar.
+colMeans(gorriones2[,2:ncol(gorriones)])
+
+#h - Ambas trazas se aumentan, la de correlciones en 1 x tener una variable mas, la de covarianzas en la varianza de la nueva variable.
+sum(diag(cov(gorriones[,2:ncol(gorriones)])))
+sum(diag(cov(gorriones2[,2:ncol(gorriones2)])))
+
+sum(diag(cor(gorriones[,2:ncol(gorriones)])))
+sum(diag(cor(gorriones2[,2:ncol(gorriones2)])))
+
+
 
 
 #2
@@ -46,7 +63,3 @@ cor(recepcionistas[,2:7])
 #d - Las trazas si son iguales porque son las covarianzas con respecto a una misma variable.
 
 
-
-
-
-#
